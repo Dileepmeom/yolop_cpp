1a2f5e691295c8003335a8c8efa25f5fce987875
Dileep Prabhu <dileep.pesit@gmail.com>
2025-01-22 00:02:36 +0530

    my first commit

---
+++ build/CMakeFiles/lite_yolop.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ridebuddy/Documents/yolop_cpp/yolop_cpp/test_lite_yolop.cpp" "/home/ridebuddy/Documents/yolop_cpp/yolop_cpp/build/CMakeFiles/lite_yolop.dir/test_lite_yolop.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ridebuddy/Documents/yolop_cpp/lite-ort1.17.1+ocv4.9.0+ffmpeg4.2.2-linux-x86_64/include"
+  "/home/ridebuddy/Documents/yolop_cpp/lite-ort1.17.1+ocv4.9.0+ffmpeg4.2.2-linux-x86_64/third_party/opencv/include/opencv4"
+  "/home/ridebuddy/Documents/yolop_cpp/lite-ort1.17.1+ocv4.9.0+ffmpeg4.2.2-linux-x86_64/third_party/onnxruntime/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/lite_yolop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lite_yolop.dir/test_lite_yolop.cpp.o"
+  "lite_yolop"
+  "lite_yolop.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lite_yolop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
