64559de313ebde85b6093bb73733bc75b34bb09c
Dileep Prabhu <dileep.pesit@gmail.com>
2025-05-09 22:37:29 +0530

    Add build configuration files and implement yolop_infer target
    - Created CMake configuration files for the yolop_infer target.
    - Added source file yolop_infer.cpp and its corresponding object file.
    - Generated dependency and flags files for proper compilation.
    - Implemented linking with ONNX Runtime and OpenCV libraries.
    - Established clean rules for build artifacts.
    - Created Makefile for build automation.
    - Added installation script for deployment.

---
+++ build/CMakeFiles/yolop_infer.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ridebuddy/Documents/yolop_cpp/yolop_cpp/yolop_infer.cpp" "/home/ridebuddy/Documents/yolop_cpp/yolop_cpp/build/CMakeFiles/yolop_infer.dir/yolop_infer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ridebuddy/Documents/libraries/onnxruntime-linux-x64-1.21.0/include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/yolop_infer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolop_infer.dir/yolop_infer.cpp.o"
+  "yolop_infer"
+  "yolop_infer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolop_infer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
